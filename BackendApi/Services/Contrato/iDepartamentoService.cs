2dbcc026680e2d0ba46e554d0997bfb39aa091ec
DaM91rp <david_rioja_paz@outlook.es>
2022-12-14 01:41:39 -0500

    Backend Api Empleados

---
+++ BackendApi/Services/Contrato/iDepartamentoService.cs
@@ -0,0 +1,9 @@
+﻿using BackendApi.Models;
+
+namespace BackendApi.Services.Contrato
+{
+    public interface iDepartamentoService
+    {
+        Task<List<Departamento>> GetList();
+    }
+}
---
+++ BackendApi/Services/Implementacion/DepartamentoService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackendApi.Models;
+using BackendApi.Services.Contrato;
+
+namespace BackendApi.Services.Implementacion
+{
+    public class DepartamentoService: iDepartamentoService
+    {
+        private DbempleadoContext _dbContext; //contexto hacia la base de datos
+
+        public DepartamentoService(DbempleadoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Departamento>>GetList()
+        {
+            try
+            {
+                List<Departamento> lista = new List<Departamento>();
+                lista = await _dbContext.Departamentos.ToListAsync();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
